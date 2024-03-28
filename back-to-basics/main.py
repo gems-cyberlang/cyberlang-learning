9cd5ff81a21ea49f9818269b6340e7d0c08aa0dd
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-03-28 19:20:12 -0400

    Format with black

--- back-to-basics/main.py
+++ back-to-basics/main.py
@@ -6,10 +6,12 @@ import time
 BASE_URL = "https://www.reddit.com"
 
 HEADERS = {
-    'User-Agent': 'cyberlang reaserach',
+    "User-Agent": "cyberlang reaserach",
 }
+
+
 class scraper_2000:
-    def __init__(self,  subreddit:str, word:str, max:int = 100) -> None:
+    def __init__(self, subreddit: str, word: str, max: int = 100) -> None:
         self.subreddit = subreddit
         self.word = word
         self.max = max
@@ -18,24 +20,31 @@ class scraper_2000:
         self.retries = 0
         self.file = open("./testFile", "+w")
 
-    def subreddit_search(self, q:str, sub_reddit:str, after:str = None, sort:str = "new", limit:int = 10, restrict_sr:bool = True):
+    def subreddit_search(
+        self,
+        q: str,
+        sub_reddit: str,
+        after: str = None,
+        sort: str = "new",
+        limit: int = 10,
+        restrict_sr: bool = True,
+    ):
         url = BASE_URL + "/r/" + sub_reddit + "/search.json"
-        params = {'q': q,
-                  'sort': sort,
-                  'limit': limit,
-                  'restrict_sr': restrict_sr}
+        params = {"q": q, "sort": sort, "limit": limit, "restrict_sr": restrict_sr}
 
-        if(after != None):
-            params['after'] = after
-        
+        if after != None:
+            params["after"] = after
+
         resp = requests.get(url, params=params, headers=HEADERS)
-        if(resp.status_code == 200):
-            self.count = self.count + int(resp.json()['data']['dist'])
+        if resp.status_code == 200:
+            self.count = self.count + int(resp.json()["data"]["dist"])
 
         return resp
-    
-    def resp_err(self, resp:requests.Response):
-        print(f'ERROR: url {resp.url}, status {resp.status_code}, content {resp.content}')
+
+    def resp_err(self, resp: requests.Response):
+        print(
+            f"ERROR: url {resp.url}, status {resp.status_code}, content {resp.content}"
+        )
         exit()
 
     def run(self):
@@ -43,34 +52,39 @@ class scraper_2000:
         time_file = open("./time.txt", "+w")
         # First get a starting point the most recent post
         resp = self.subreddit_search(self.word, self.subreddit, limit=1)
-        
-        if(resp.status_code != 200): self.resp_err()
+
+        if resp.status_code != 200:
+            self.resp_err()
 
         json.dump(resp.json(), raw_file, indent=True)
-        item = resp.json()['data']['children'][0]
+        item = resp.json()["data"]["children"][0]
 
-        print(time.ctime(item['data']['created']))
-        time_file.write(time.ctime(item['data']['created']) + "\n")
+        print(time.ctime(item["data"]["created"]))
+        time_file.write(time.ctime(item["data"]["created"]) + "\n")
 
-        curr_after = item['data']['name']
+        curr_after = item["data"]["name"]
 
         while self.count < self.max:
-            resp =  self.subreddit_search(self.word, self.subreddit, after=curr_after, limit=100)
-            while(resp.status_code != 200 and self.retries < self.max_retries):
-                if(resp.status_code == 409): # to many requests 
-                    time.sleep(6000) 
+            resp = self.subreddit_search(
+                self.word, self.subreddit, after=curr_after, limit=100
+            )
+            while resp.status_code != 200 and self.retries < self.max_retries:
+                if resp.status_code == 409:  # to many requests
+                    time.sleep(6000)
                     print("Waiting")
 
-                resp =  self.subreddit_search(self.word, self.subreddit, after=curr_after, limit=100)
+                resp = self.subreddit_search(
+                    self.word, self.subreddit, after=curr_after, limit=100
+                )
 
                 self.retries += 1
-            
-            if(self.retries == self.max_retries):
+
+            if self.retries == self.max_retries:
                 json.dump(resp.json(), raw_file, indent=True)
 
-                for post in resp.json()['data']['children']:
-                    print(time.ctime(post['data']['created']))
-                    time_file.write(time.ctime(post['data']['created']) + "\n")
+                for post in resp.json()["data"]["children"]:
+                    print(time.ctime(post["data"]["created"]))
+                    time_file.write(time.ctime(post["data"]["created"]) + "\n")
 
                 print("reties exeded leaving")
                 exit()
@@ -79,20 +93,31 @@ class scraper_2000:
 
             json.dump(resp.json(), raw_file, indent=True)
 
-            for post in resp.json()['data']['children']:
-                print(time.ctime(post['data']['created']))
-                time_file.write(time.ctime(post['data']['created']) + "\n")
+            for post in resp.json()["data"]["children"]:
+                print(time.ctime(post["data"]["created"]))
+                time_file.write(time.ctime(post["data"]["created"]) + "\n")
 
-            item = resp.json()['data']['children'][0]
-            curr_after = item['data']['name']
+            item = resp.json()["data"]["children"][0]
+            curr_after = item["data"]["name"]
 
         raw_file.close()
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--subreddit', metavar='SUBREDDIT_NAME', help='Subreddit to get data from', default="unix")
-    parser.add_argument('--query', help='Search term', default="unix")
-    parser.add_argument('--end', metavar='ID', help='ID of the last post, to search from backwards', default=None)
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "--subreddit",
+        metavar="SUBREDDIT_NAME",
+        help="Subreddit to get data from",
+        default="unix",
+    )
+    parser.add_argument("--query", help="Search term", default="unix")
+    parser.add_argument(
+        "--end",
+        metavar="ID",
+        help="ID of the last post, to search from backwards",
+        default=None,
+    )
     args = parser.parse_args()
     scrapy = scraper_2000(args.subreddit, args.query, max=500)
     scrapy.run()
