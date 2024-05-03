637d096d202e54bbdfc580efb46f91f072bbaa9c
O-Despo <oliverdesposito@gmail.com>
2024-05-03 12:18:29 -0400

    started adding np

--- data-collection/main.py
+++ data-collection/main.py
@@ -10,6 +10,7 @@ import praw
 import json
 from typing import Optional
 import sys
+import numpy.typing as npt
 
 import praw.exceptions
 import praw.models
@@ -22,6 +23,14 @@ LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 get_formatted_time = lambda: time.strftime("%Y-%m-%d-%H-%M-%S")
 
+class permutaion:
+    def __init__(
+        self,
+        start: int,
+        stop: int,
+        
+        ) -> None:
+        pass
 
 class gems_runner:
     def __init__(
@@ -51,6 +60,7 @@ class gems_runner:
         self.step = step
         self.client_id = client_id
         self.reddit_secret = reddit_secret
+        self.overwrite = overwrite
 
         # Open files and directores loading data from there
         if not os.path.isdir(output_folder):
@@ -58,7 +68,7 @@ class gems_runner:
 
         mode = "w+" if overwrite else "a+"
 
-        write_rows = overwrite or not os.path.isfile(
+        write_rows = overwrite or not os.path.isfile( # Come back and fix this 
             os.path.join(output_folder, "comments.csv")
         )
 
@@ -95,6 +105,12 @@ class gems_runner:
         )
 
         self.logger.info("init complete")
+
+        # file getters
+        self.get_perm_file_name = lambda start, stop: os.path.join(self.output_dur, f"perm-{str(start)}-{str(stop)}.perm")
+        self.get_pos_file_name = lambda start, stop: os.path.join(self.output_dur, f"pos-{str(start)}-{str(stop)}.perm")
+
+
 
     def crit_err(self, err_msg: str, logger: logging.Logger):
         """logs error and kills program
@@ -151,7 +167,43 @@ class gems_runner:
 
         return logger
 
-    def run_sub_section(self, start, stop, max):
+    def get_perm(self, start:int, stop:int, max: int): 
+        """ Will get a permutaion if its exists if it dose not will open and save a new perm.
+        Note perm revocery is only possible if the SIZE_OF_ITER and other constants are exactly
+        the same as the prior run.
+
+        Args:
+            start (int): start index of perm
+            stop (int): end index of perm 
+            max (int): the max number of requests to get form the perm
+
+        Returns:
+            (perm: np.array dtype=np.unint64, pos np.array dtype=np.uint64): the perm and pos 
+        """
+        perm_file_name = self.get_perm_file_name(start, stop)
+        pos_file_name = self.get_pos_file_name(start, stop)
+
+        perm_file_exsits = os.path.isfile(perm_file_name)
+        pos_file_exsits = os.path.isfile(pos_file_name)
+
+        if self.overwrite or not perm_file_exsits or not pos_file_exsits:
+            perm = np.random.permutation(np.arange(start=start, stop=stop, dtype=np.uint64)) 
+            pos = np.array([0], dtype=np.uint64)
+
+            np.savetxt(perm_file_name, perm)
+            np.savetxt(pos_file_name, pos)
+        else:
+            perm = np.loadtxt(perm_file_name, dtype=np.uint64)
+            pos = np.loadtxt(pos_file_name, dtype=np.uint64)
+
+        return perm, pos, pos_file_name
+
+    def update_pos (self, pos:np.array) {
+        pos_file_name = slef.get_pos_file_name()
+        np.savetext(pos_file_name)
+    }
+
+    def run_sub_section(self, start: int, stop: int, max: int):
         """Runs the full system for the based on inisialized values."""
         sub_count = 0
         make_request_str_from_id = lambda id: f"t1_{np.base_repr(id, 36).lower()}"
