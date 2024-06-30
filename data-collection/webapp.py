b87b5ec6b783815fc73c82fb74b3a12896e9fd84
ysthakur <45539777+ysthakur@users.noreply.github.com>
2024-06-30 14:58:49 -0400

    Add a msises graph

--- data-collection/webapp.py
+++ data-collection/webapp.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import streamlit as st
-import time
 
 from runner import gems_runner
 
 
 def run_app(runner: gems_runner):
-    with st.empty():
-        while runner.run_step():
-            df = pd.DataFrame(
-                [
-                    [str(time_range.start_date), time_range.hits]
-                    for time_range in runner.time_ranges.bins
-                ],
-                columns=["Date", "Hits"],
-            )
-            st.bar_chart(df, x="Date", y="Hits")
+    hits_graph = st.empty()
+    misses_graph = st.empty()
+    while runner.run_step():
+        df = pd.DataFrame(
+            [
+                [str(time_range.start_date), time_range.min, time_range.hits, time_range.misses]
+                for time_range in runner.time_ranges.bins
+            ],
+            columns=["Date", "Min", "Hits", "Misses"],
+        )
+        hits_graph.bar_chart(df, x="Date", y="Hits")
+        misses_graph.bar_chart(df, x="Date", y="Misses")
+    st.write("Done")
